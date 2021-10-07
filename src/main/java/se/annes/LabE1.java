82760baae55fd4a4e24afec31bc3c21e2a83a6b3
anne <anne.soder82@gmail.com>
2021-10-07 14:12:00 +0200

    bbb

---
+++ src/main/java/se/annes/LabE1.java
@@ -0,0 +1,20 @@
+package se.annes;
+
+public class LabE1 {
+    public void siffror(){
+/*Skapa en ARRAY i kod med INTS 12 ,55, 32, 9, 34.   
+Ta fram vilket som är det STÖRSTA och MINSTA talet i arrayen*/
+
+
+int [] minaNummer = {12 ,55, 32, 9, 34};
+
+System.out.println(minaNummer[2]);
+
+int largest = minaNummer [0];
+
+
+
+}
+
+    }
+
---
+++ src/main/java/se/annes/LabE2.java
@@ -0,0 +1,15 @@
+package se.annes;
+
+public class LabE2 {
+     public void compare(){
+
+/*Skapa en ArrayList i kod med INTS 12 ,55, 32, 9, 34.    Tips 
+ ArrayList<Integer> tal1 = new ArrayList<>(Arrays.asList( 12,55,32,9,34));
+Skapa en ArrayList till i kod med INTS 72 ,55, 32, 4, 88.   
+Skapa en ArrayList - som heter commonArrayList. Denna ska nu fyllas med  med tal som återfinns i BÅDA arraylistorna. Loopa igenom den första och för varje: kolla om den finns i den andra. Isåfall stoppar du in den i common ArrayListen.
+Loopa igenom common  ArrayListen och skriv ut alla tal*/
+
+
+
+     }
+}
