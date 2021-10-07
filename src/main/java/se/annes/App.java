65d93def3998ee5d5f439c5286ac069ceec68b7c
anne <anne.soder82@gmail.com>
2021-10-07 14:11:08 +0200

    aaa

---
+++ src/main/java/se/annes/App.java
@@ -0,0 +1,24 @@
+package se.annes;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        
+
+        //LabE1 labbet = new LabE1();
+        //labbet.siffror();
+
+
+        //LabE3 summan = new LabE3();
+        //summan.addition();
+    }
+}
